915959952a138bff2c8aeedcebb87ed229846b57
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2016-03-23 14:03:56 -0300

    Menu

--- MC1/Menu/MainMenuViewController.swift
+++ MC1/Menu/MainMenuViewController.swift
@@ -10,21 +10,47 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
     
-    // MARK:
+    private enum Segue: String, CustomStringConvertible {
+        case SinglePlayer = "gotoSinglePlayer"
+        case Multiplayer = "gotoMultiplayerMenu"
+        
+        var description: String {
+            return self.rawValue
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        guard let segueIdentifier = segue.identifier, let _ = Segue(rawValue: segueIdentifier) else {
+            return
+        }
+        
+        switch Segue(rawValue: segueIdentifier)! {
+        case Segue.Multiplayer:
+            let vc = segue.destinationViewController
+            presentViewController(vc, animated: true, completion: nil)
+            
+        case Segue.SinglePlayer:
+            print("gotoSinglePlayer")
+        }
+    }
+    
+    // MARK: IBActions
     @IBAction func singlePlayerBtnTouched(sender: AnyObject) {
         gotoSinglePlayer()
     }
-
+    
     @IBAction func multiPlayerBtnTouched(sender: AnyObject) {
         gotoMultiplayer()
     }
     
+    // MARK: Private helpers
     private func gotoSinglePlayer() {
         print("Single player")
     }
     
     private func gotoMultiplayer() {
-        print("Multiplayer player")
+        print("Multiplayer")
+        performSegueWithIdentifier(Segue.Multiplayer.description, sender: nil)
     }
     
 }
---
+++ MC1/Menu/MultiplayerMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MultiplayerMenuViewController.swift
+//  MC1
+//
+//  Created by Henrique Valcanaia on 3/23/16.
+//  Copyright © 2016 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class MultiplayerMenuViewController: UIViewController {
+
+}
